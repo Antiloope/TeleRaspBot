80c0f981248b86765bc34ea1c5b5c1397a8b5d65
Antiloope <rodrigopizarro1234@gmail.com>
2019-03-05 23:42:38 -0300

    Agregado inicio de octoprint

--- api/controllers/ReciveController.js
+++ api/controllers/ReciveController.js
@@ -7,16 +7,17 @@ const bot = new TelegramBot(token, {polling: true});
 
 const shell = require('shelljs');
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/octoprint start/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  shell.exec('~/OctoPrint/venv/bin/octoprint serve');
+  //const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  bot.sendMessage(chatId, 'Octoprint server started u\'U0001F525\'');
 });
 
 // Listen for any kind of message. There are different kinds of
